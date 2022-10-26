d59343ec163ada5a6ede5ba27a8010b77de52ef6
CrystalQuest112 <114012059+CrystalQuest112@users.noreply.github.com>
2022-10-26 18:01:55 +0200

    Integrated  Dictionary API

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,12 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./dictionary.css";
 
 function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+
+  function defineWord(response) {
+    console.log(response.data[0]);
+  }
+
+  function searchWord(event) {
+    setKeyword(event.target.value);
+  }
+
+  function getWord(event) {
+    event.preventDefault();
+    alert(`${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(defineWord);
+  }
+
   return (
     <div className="box1">
       <div className="card mb-3">
         <div className="card-body">
-          <form>
-            <input type="search" placeholder="find word"></input>
+          <form onSubmit={getWord}>
+            <input
+              type="search"
+              placeholder="find word"
+              onChange={searchWord}
+            ></input>
             <input type="submit" value="search"></input>
           </form>
           <br />
@@ -21,7 +45,7 @@ function Dictionary() {
             </p>
           </div>
           <h1 className="bigword">uraeus</h1>{" "}
-          <small class="text-muted" id="small">
+          <small className="text-muted" id="small">
             [ yoo-ree-uhs ]
           </small>
         </div>
